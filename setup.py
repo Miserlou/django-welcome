4c388565083aff76089cb88641fd327326bdb2a4
Rich Jones <rich@anomos.info>
2013-10-13 18:57:55 -0700

    manifestation

---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-welcome',
+    version='0.1.0',
+    packages=['welcome'],
+    install_requires=[
+    ],
+    include_package_data=True,
+    license='MIT License',
+    description='Send email messages to new users.',
+    long_description=README,
+    url='https://github.com/Miserlou/django-welcome',
+    author='Rich Jones',
+    author_email='rich@openwatch.net',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
